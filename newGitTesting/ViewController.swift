92abc78b1581228707550a32638955f4be43e7ed
Grady Vickery <gradyvickery@gradys-macbook-air.local>
2017-01-08 12:21:53 -0600

    Initial Commit

---
+++ newGitTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newGitTesting
+//
+//  Created by Grady Vickery on 1/8/17.
+//  Copyright © 2017 White Oak Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
